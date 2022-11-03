d3356dcd059cab10b4c4edf49fd1a7b6b479ebc9
Eduardo Guette <eduardoguette@gmail.com>
2022-11-03 23:28:48 +0100

    add limit results

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
   const APIKey = 'fzId3VBuREjibqafqpDWOufrEfE0rw3r'
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${category}&limit=25&offset=0&rating=g`
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${category}&limit=100&offset=0&rating=g`
   const resp = await fetch(url)
   const { data } = await resp.json()
   const gifs = data.map((img) => ({
